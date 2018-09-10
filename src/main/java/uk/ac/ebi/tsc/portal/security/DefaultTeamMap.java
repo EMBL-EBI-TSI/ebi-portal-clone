2718031c67d50ee6f7ec84dc3e80bc44de2d5a1b
Jose A Dianes <jadianes@gmail.com>
2018-09-10 14:40:51 +0100

    first prototype of externally managed team mappings

---
+++ src/main/java/uk/ac/ebi/tsc/portal/security/DefaultTeamMap.java
@@ -0,0 +1,25 @@
+package uk.ac.ebi.tsc.portal.security;
+
+/**
+ * Created by jdianes on 10/09/2018.
+ */
+public class DefaultTeamMap {
+    private String teamName;
+    private String emailDomain;
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public String getEmailDomain() {
+        return emailDomain;
+    }
+
+    public void setEmailDomain(String emailDomain) {
+        this.emailDomain = emailDomain;
+    }
+}
