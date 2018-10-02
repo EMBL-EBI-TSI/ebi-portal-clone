f07d6b7589516bbdd78cdae6dc23299f4a5a2e6d
Jose A Dianes <jadianes@gmail.com>
2018-10-02 13:26:50 +0100

    factored out abstract class and helper from application deployer

---
+++ src/main/java/uk/ac/ebi/tsc/portal/clouddeployment/application/ApplicationDeployer.java
@@ -0,0 +1,218 @@
+package uk.ac.ebi.tsc.portal.clouddeployment.application;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.client.RestClientException;
+import uk.ac.ebi.tsc.aap.client.repo.DomainService;
+import uk.ac.ebi.tsc.portal.api.application.repo.Application;
+import uk.ac.ebi.tsc.portal.api.application.repo.ApplicationOutput;
+import uk.ac.ebi.tsc.portal.api.application.repo.ApplicationRepository;
+import uk.ac.ebi.tsc.portal.api.application.service.ApplicationService;
+import uk.ac.ebi.tsc.portal.api.cloudproviderparameters.repo.CloudProviderParamsCopy;
+import uk.ac.ebi.tsc.portal.api.cloudproviderparameters.repo.CloudProviderParamsCopyRepository;
+import uk.ac.ebi.tsc.portal.api.cloudproviderparameters.service.CloudProviderParamsCopyService;
+import uk.ac.ebi.tsc.portal.api.configuration.repo.ConfigDeploymentParamsCopyRepository;
+import uk.ac.ebi.tsc.portal.api.configuration.repo.Configuration;
+import uk.ac.ebi.tsc.portal.api.configuration.service.ConfigDeploymentParamsCopyService;
+import uk.ac.ebi.tsc.portal.api.deployment.repo.*;
+import uk.ac.ebi.tsc.portal.api.deployment.service.DeploymentService;
+import uk.ac.ebi.tsc.portal.api.encryptdecrypt.security.EncryptionService;
+import uk.ac.ebi.tsc.portal.clouddeployment.exceptions.ApplicationDeployerException;
+import uk.ac.ebi.tsc.portal.clouddeployment.model.ErrorFromTerraformOutput;
+import uk.ac.ebi.tsc.portal.clouddeployment.model.StateFromTerraformOutput;
+import uk.ac.ebi.tsc.portal.clouddeployment.model.terraform.TerraformState;
+import uk.ac.ebi.tsc.portal.usage.deployment.model.DeploymentDocument;
+import uk.ac.ebi.tsc.portal.usage.deployment.service.DeploymentIndexService;
+
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * Created by jdianes on 01/10/2018.
+ */
+public abstract class ApplicationDeployer {
+
+    private static final Logger logger = LoggerFactory.getLogger(ApplicationDeployer.class);
+
+    protected final DeploymentService deploymentService;
+    protected final ApplicationService applicationService;
+    protected final CloudProviderParamsCopyService cloudProviderParametersCopyService;
+    protected final ConfigDeploymentParamsCopyService configDeploymentParamsCopyService;
+
+    public ApplicationDeployer(DeploymentRepository deploymentRepository,
+                               DeploymentStatusRepository deploymentStatusRepository,
+                               ApplicationRepository applicationRepository,
+                               DomainService domainService,
+                               CloudProviderParamsCopyRepository cloudProviderParametersRepository,
+                               ConfigDeploymentParamsCopyRepository configDeploymentParamsCopyRepository,
+                               EncryptionService encryptionService) {
+        this.deploymentService = new DeploymentService(deploymentRepository, deploymentStatusRepository);
+        this.applicationService = new ApplicationService(applicationRepository, domainService);
+        this.cloudProviderParametersCopyService = new CloudProviderParamsCopyService(cloudProviderParametersRepository, encryptionService);
+        this.configDeploymentParamsCopyService = new ConfigDeploymentParamsCopyService(configDeploymentParamsCopyRepository);
+
+    }
+    public abstract void deploy(String userEmail,
+                                Application theApplication,
+                                String reference,
+                                String cloudProviderPath,
+                                Map<String, String> inputAssignments,
+                                Map<String, String> parameterAssignments,
+                                Map<String, String> volumeAttachments,
+                                Map<String, String> configurationParameters,
+                                CloudProviderParamsCopy cloudProviderParametersCopy,
+                                Configuration configuration,
+                                java.sql.Timestamp startTime,
+                                String userSshKey)  throws IOException,
+            ApplicationDeployerException, NoSuchAlgorithmException, NoSuchProviderException;
+
+    public abstract StateFromTerraformOutput state(String repoPath,
+                                          String reference,
+                                          String cloudProviderPath,
+                                          Map<String, String> outputs,
+                                          Configuration configuration,
+                                          String userSshKey) throws IOException, ApplicationDeployerException;
+
+    public abstract void destroy(String repoPath, String reference,
+                 String cloudProviderPath,
+                 Collection<DeploymentAssignedInput> inputAssignments,
+                 Collection<DeploymentAssignedParameter> parameterAssignments,
+                 Collection<DeploymentAttachedVolume> volumeAttachments,
+                 DeploymentConfiguration deploymentConfiguration,
+                 CloudProviderParamsCopy cloudProviderParametersCopy) throws IOException,
+            ApplicationDeployerException;
+
+
+    protected void updateDeploymentStatus(DeploymentIndexService deploymentIndexService,
+                                          Deployment theDeployment,
+                                        DeploymentStatusEnum status, String cause, TerraformState terraformState, String terraformOutput,
+                                        java.sql.Timestamp startTime) {
+        // update status
+        theDeployment.getDeploymentStatus().setStatus(status);
+        // persist changes
+        this.deploymentService.save(theDeployment);
+
+        CloudProviderParamsCopy cloudProviderParametersCopy = this.cloudProviderParametersCopyService.
+                findByCloudProviderParametersReference(theDeployment.getCloudProviderParametersReference());
+
+
+        // Index deployment
+        if (deploymentIndexService != null) {
+            DeploymentDocument theDeploymentDocument = deploymentIndexService.findById(theDeployment.getDomainReference());
+            if (theDeploymentDocument == null) {
+                theDeploymentDocument = new DeploymentDocument(
+                        theDeployment.getAccount().getEmail(),
+                        theDeployment.getReference(),
+                        theDeployment.getDeploymentApplication().getName(),
+                        theDeployment.getDeploymentApplication().getContact(),
+                        theDeployment.getDeploymentApplication().getVersion(),
+                        theDeployment.getDeploymentApplication().getName(),
+                        cloudProviderParametersCopy.cloudProvider);
+            }
+            // set resources if provided
+            if (terraformState!=null) {
+                ApplicationDeployerHelper.updateResourceConsumptionFromTerraformState(terraformState, theDeploymentDocument);
+            }
+            // set status
+            theDeploymentDocument.setStatus(status.toString());
+            switch (status) {
+                case STARTING:
+                    theDeploymentDocument.setStartedTime(new Date(System.currentTimeMillis()));
+                    //start time
+                    theDeployment.setStartTime(startTime);
+                    break;
+                case RUNNING:
+                    //deployed time
+                    theDeploymentDocument.setDeployedTime(new Date(System.currentTimeMillis()));
+                    theDeployment.setDeployedTime(new java.sql.Timestamp(System.currentTimeMillis()));
+                    break;
+                case DESTROYED:
+                    theDeploymentDocument.setDestroyedTime(new Date(System.currentTimeMillis()));
+                    theDeployment.setDestroyedTime(new java.sql.Timestamp(System.currentTimeMillis()));
+                    if (theDeploymentDocument.getDeployedTime()!=null) {
+                        theDeploymentDocument.setTotalRunningTime(
+                                theDeploymentDocument.getDestroyedTime().getTime() - theDeploymentDocument.getDeployedTime().getTime()
+                        );
+                    }
+                    break;
+                case STARTING_FAILED:
+                case RUNNING_FAILED:
+                case DESTROYING_FAILED:
+                    //start time, would be null for detroying_failed
+                    if(startTime != null){
+                        theDeployment.setStartTime(startTime);
+                    }
+                    theDeploymentDocument.setFailedTime(new Date(System.currentTimeMillis()));
+                    theDeployment.setFailedTime(new java.sql.Timestamp(System.currentTimeMillis()));
+                    theDeploymentDocument.setErrorCause(cause);
+                    ErrorFromTerraformOutput error = ApplicationDeployerHelper.errorFromTerraformOutput(terraformOutput, logger);
+                    theDeploymentDocument.setErrorCause(error.getErrorMessage());
+                    break;
+            }
+            try {
+                deploymentIndexService.save(theDeploymentDocument);
+            } catch (RestClientException rce) {
+                logger.error("DeploymentIndex service not available. Cause: ");
+                rce.printStackTrace();
+            }
+            logger.info("Saving deployment status times");
+            this.deploymentService.save(theDeployment);
+        }
+
+    }
+
+
+    protected void updateDeploymentOutputs(DeploymentIndexService deploymentIndexService, ApplicationService applicationService,
+                                         Deployment theDeployment, String cloudProviderPath,
+                                         DeploymentStatusEnum failStatus, Configuration configuration, String terraformOutput) {
+        Application theApplication = applicationService.findByAccountUsernameAndName(
+                theDeployment.getDeploymentApplication().getAccount().getUsername(),
+                theDeployment.getDeploymentApplication().getName());
+
+        // get deployment state
+        Map<String,String> deploymentOutputs = theApplication.getOutputs().stream()
+                .collect(Collectors.toMap(ApplicationOutput::getName, s -> ""));
+        StateFromTerraformOutput stateFromTerraformOutput = null;
+        try {
+            stateFromTerraformOutput = this.state(
+                    theDeployment.getDeploymentApplication().getRepoPath(),
+                    theDeployment.getReference(),
+                    cloudProviderPath,
+                    deploymentOutputs,
+                    configuration,
+                    theDeployment.getUserSshKey()
+            );
+
+
+            // update the deployment
+            theDeployment.setAccessIp(stateFromTerraformOutput.getAccessIp());
+            theDeployment.setProviderId(stateFromTerraformOutput.getId()!=null ? stateFromTerraformOutput.getId():"");
+
+            // set outputs
+            theApplication.getOutputs().forEach(output ->  {
+//                        logger.debug( "Adding output " + output.getName() + " = " + deploymentOutputs.get(output.getName()) );
+                        //                        logger.info( "  with length " + deploymentOutputs.get(output.getName()).length());
+                        DeploymentGeneratedOutput newOutput = new DeploymentGeneratedOutput(
+                                output.getName(),
+                                deploymentOutputs.get(output.getName()),
+                                theDeployment
+                        );
+                        //                        logger.info( "  with new length " + newOutput.getValue().length());
+                        theDeployment.getGeneratedOutputs().add(newOutput);
+                    }
+            );
+
+        } catch (IOException | ApplicationDeployerException e) {
+            e.printStackTrace();
+            // update status
+            this.updateDeploymentStatus(deploymentIndexService, theDeployment,
+                    failStatus,"Failed to obtain outputs from state file", null, terraformOutput, null);
+        }
+
+    }
+}
---
+++ src/main/java/uk/ac/ebi/tsc/portal/clouddeployment/application/ApplicationDeployerHelper.java
@@ -0,0 +1,204 @@
+package uk.ac.ebi.tsc.portal.clouddeployment.application;
+
+import org.slf4j.Logger;
+import uk.ac.ebi.tsc.portal.api.cloudproviderparameters.repo.CloudProviderParamsCopy;
+import uk.ac.ebi.tsc.portal.api.cloudproviderparameters.repo.CloudProviderParamsCopyField;
+import uk.ac.ebi.tsc.portal.clouddeployment.model.ErrorFromTerraformOutput;
+import uk.ac.ebi.tsc.portal.clouddeployment.model.MachineSpecs;
+import uk.ac.ebi.tsc.portal.clouddeployment.model.StateFromTerraformOutput;
+import uk.ac.ebi.tsc.portal.clouddeployment.model.terraform.TerraformModule;
+import uk.ac.ebi.tsc.portal.clouddeployment.model.terraform.TerraformResource;
+import uk.ac.ebi.tsc.portal.clouddeployment.model.terraform.TerraformState;
+import uk.ac.ebi.tsc.portal.usage.deployment.model.DeploymentDocument;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by jdianes on 26/09/2018.
+ */
+public class ApplicationDeployerHelper {
+
+    private static final String OS_FLOATING_IP = "floating_ip";
+    private static final String ERROR_MESSAGE = "error(s) occurred";
+    private static final String IMAGE_NAME_ERROR_MESSAGE = "Error resolving image name";
+    private static final String TIMEOUT_ERROR_MESSAGE = "Error waiting for instance";
+    private static final String QUOTA_EXCEEDED_ERROR_MESSAGE = "Quota exceeded for ";
+
+    public static String getOutputFromFile(File file, Logger logger) {
+        logger.info("Retrieving output for process from " + file.getAbsolutePath());
+
+        String output = null;
+        try {
+            output = new Scanner(file).useDelimiter("\\Z").next();
+        } catch (FileNotFoundException e) {
+            logger.error("Can't read output from file " + file.getAbsolutePath());
+            e.printStackTrace();
+        }
+
+        return output;
+    }
+
+
+    public static void addGenericProviderCreds(Map<String, String> env, CloudProviderParamsCopy cloudProviderCredentialsCopy, Logger logger) {
+        logger.info("Setting Cloud Provider Parameters Copy for " + cloudProviderCredentialsCopy.getName());
+
+        for (CloudProviderParamsCopyField cloudProviderParametersCopyField : cloudProviderCredentialsCopy.getFields()) {
+            logger.info("Setting " + cloudProviderParametersCopyField.getKey());
+            env.put(cloudProviderParametersCopyField.getKey(), cloudProviderParametersCopyField.getValue());
+        }
+    }
+
+    public static StateFromTerraformOutput terraformStateFromString(String output, Map<String, String> outputs, Logger logger) throws IOException {
+
+        logger.info("The whole terraform state is: " + output);
+
+        StateFromTerraformOutput stateFromTerraformOutput = new StateFromTerraformOutput();
+
+        String[] lines = output.split(System.getProperty("line.separator"));
+
+        // Read Id, it should be the first ID (TODO improve this later)
+        stateFromTerraformOutput.setId(lines[1].replaceAll(" ","").split("=")[1]);
+        // look for the IP
+        Pattern osFloatingIpPattern = Pattern.compile(OS_FLOATING_IP);
+        boolean ipFound = false;
+        int i=1;
+        while (!ipFound && i<lines.length) {
+            String line = lines[i].replaceAll(" ","");
+            Matcher osFloatingIpMatcher = osFloatingIpPattern.matcher(line);
+            if (osFloatingIpMatcher.lookingAt()) {
+                // Read IP
+                String ip = line.split("=")[1].replaceAll("\n", "").replaceAll("\r", "");
+                logger.debug("There is a match. IP = " + ip);
+                stateFromTerraformOutput.setAccessIp(ip);
+                ipFound = true;
+            }
+            i++;
+        }
+        // parse for any other outputs
+        if (outputs != null) {
+            getOutputs(lines, outputs, logger);
+        }
+
+        return stateFromTerraformOutput;
+    }
+
+    public static void getOutputs(String[] lines, Map<String, String> outputs, Logger logger) {
+        for (int i = 0; i<lines.length; i++) {
+            String line = lines[i].replaceAll(" ","");
+            String[] lineSplit = line.split("=");
+            if ( outputs.containsKey(lineSplit[0]) ) {
+                outputs.put(lineSplit[0], lineSplit[1].replaceAll("\n", "").replaceAll("\r", "").replaceAll("\\[0m","").trim());
+                logger.debug("There is a match. " + lineSplit[0] + " = " + outputs.get(lineSplit[0]));
+            }
+        }
+    }
+
+    public static ErrorFromTerraformOutput errorFromTerraformOutput(String terraformOutput, Logger logger) {
+        ErrorFromTerraformOutput res = new ErrorFromTerraformOutput();
+        res.setErrorMessage("unknown");
+
+        // look for specific error message
+
+        if (terraformOutput.contains(TIMEOUT_ERROR_MESSAGE)) {
+            logger.info("Timeout error");
+            res.setErrorMessage("timeout");
+            return res;
+        }
+
+        if (terraformOutput.contains(QUOTA_EXCEEDED_ERROR_MESSAGE)) {
+            logger.info("Quota exceeded error");
+            res.setErrorMessage("quota exceeded");
+            return res;
+        }
+
+        if (terraformOutput.contains(IMAGE_NAME_ERROR_MESSAGE)) {
+            logger.info("Error resolving image name");
+            res.setErrorMessage("unresolvable image name");
+            return res;
+        }
+
+        return res;
+    }
+
+    public static long updateResourceConsumptionFromTerraformState(TerraformState terraformState, DeploymentDocument deploymentDocument) {
+        long res = 0;
+        if (terraformState.modules!=null && terraformState.modules.length>0) {
+            Iterator<TerraformModule> itModules = Arrays.stream(terraformState.modules).iterator();
+            while (itModules.hasNext()) {
+                TerraformModule terraformModule = itModules.next();
+                Iterator<Map.Entry<String, TerraformResource>> it = terraformModule.resources.entrySet().iterator();
+                while (it.hasNext()) {
+                    TerraformResource terraformResource = it.next().getValue();
+                    if (terraformResource.type.equals("openstack_compute_instance_v2")) {
+                        deploymentDocument.setInstanceCount(deploymentDocument.getInstanceCount()+1);
+                        deploymentDocument.setTotalVcpus(
+                                deploymentDocument.getTotalVcpus()
+                                        + MachineSpecs.fromFlavourName(terraformResource.primary.attributes.get("flavor_name")).getvCPUSs()
+                        );
+                        deploymentDocument.setTotalRamGb(
+                                deploymentDocument.getTotalRamGb()
+                                        + MachineSpecs.fromFlavourName(terraformResource.primary.attributes.get("flavor_name")).getRamGb()
+                        );
+                        deploymentDocument.setTotalDiskGb(
+                                deploymentDocument.getTotalDiskGb()
+                                        + MachineSpecs.fromFlavourName(terraformResource.primary.attributes.get("flavor_name")).getDiskSpaceGb()
+                        );
+                    } else if (terraformResource.type.equals("google_compute_instance")) {
+                        deploymentDocument.setInstanceCount(deploymentDocument.getInstanceCount()+1);
+                        deploymentDocument.setTotalVcpus(
+                                deploymentDocument.getTotalVcpus()
+                                        + MachineSpecs.fromFlavourName(terraformResource.primary.attributes.get("machine_type")).getvCPUSs()
+                        );
+                        deploymentDocument.setTotalRamGb(
+                                deploymentDocument.getTotalRamGb()
+                                        + MachineSpecs.fromFlavourName(terraformResource.primary.attributes.get("machine_type")).getRamGb()
+                        );
+                        deploymentDocument.setTotalDiskGb(
+                                deploymentDocument.getTotalDiskGb()
+                                        + MachineSpecs.fromFlavourName(terraformResource.primary.attributes.get("machine_type")).getDiskSpaceGb()
+                        );
+                    } else if (terraformResource.type.equals("aws_instance")) {
+                        deploymentDocument.setInstanceCount(deploymentDocument.getInstanceCount()+1);
+                        deploymentDocument.setTotalVcpus(
+                                deploymentDocument.getTotalVcpus()
+                                        + MachineSpecs.fromFlavourName(terraformResource.primary.attributes.get("instance_type")).getvCPUSs()
+                        );
+                        deploymentDocument.setTotalRamGb(
+                                deploymentDocument.getTotalRamGb()
+                                        + MachineSpecs.fromFlavourName(terraformResource.primary.attributes.get("instance_type")).getRamGb()
+                        );
+                        deploymentDocument.setTotalDiskGb(
+                                deploymentDocument.getTotalDiskGb()
+                                        + MachineSpecs.fromFlavourName(terraformResource.primary.attributes.get("instance_type")).getDiskSpaceGb()
+                        );
+                    } else if (terraformResource.type.equals("azurerm_virtual_machine")) {
+                        deploymentDocument.setInstanceCount(deploymentDocument.getInstanceCount()+1);
+                        deploymentDocument.setTotalVcpus(
+                                deploymentDocument.getTotalVcpus()
+                                        + MachineSpecs.fromFlavourName(terraformResource.primary.attributes.get("vm_size")).getvCPUSs()
+                        );
+                        deploymentDocument.setTotalRamGb(
+                                deploymentDocument.getTotalRamGb()
+                                        + MachineSpecs.fromFlavourName(terraformResource.primary.attributes.get("vm_size")).getRamGb()
+                        );
+                        deploymentDocument.setTotalDiskGb(
+                                deploymentDocument.getTotalDiskGb()
+                                        + MachineSpecs.fromFlavourName(terraformResource.primary.attributes.get("vm_size")).getDiskSpaceGb()
+                        );
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
+
+}
