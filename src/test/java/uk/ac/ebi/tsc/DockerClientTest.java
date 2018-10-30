362eb5f9e8b1f374f9b9b52ac7dc272f641dc723
Thiago Albuquerque <thiagofga@gmail.com>
2018-10-30 15:54:17 +0000

    [#4] hello_world: removing the container afterwards
    * Now I remove the container after it finishes; and
    * Assert that the number of containers (retuned by `docker ps -a`) didn't change.

--- src/test/java/uk/ac/ebi/tsc/DockerClientTest.java
+++ src/test/java/uk/ac/ebi/tsc/DockerClientTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 
 import com.spotify.docker.client.DefaultDockerClient;
 import com.spotify.docker.client.DockerClient;
+import com.spotify.docker.client.DockerClient.ListContainersParam;
 import com.spotify.docker.client.exceptions.DockerException;
 import com.spotify.docker.client.messages.Container;
 import com.spotify.docker.client.messages.ContainerConfig;
@@ -33,9 +34,45 @@ public class DockerClientTest {
         
         System.out.println(containers.size());
     }
+
+
+    int numContainers() throws DockerException, InterruptedException {
+        
+        List<Container> containers = docker.listContainers(ListContainersParam.allContainers());
+        
+        return containers.size();
+    }
+    
+    @Test
+    public void ps_with_filter() throws DockerException, InterruptedException {
+        
+        List<Container> containers = listContainersByName("tfga");
+        
+        System.out.printf("Containers = %d\n", containers.size());
+    }
+
+
+    /**
+     * docker ps -a --filter 'name={partOfName}'
+     * 
+     */
+    List<Container> listContainersByName(String partOfName) throws DockerException, InterruptedException {
+        
+        List<Container> containers = docker.listContainers(
+                
+             ListContainersParam.allContainers()
+            ,ListContainersParam.filter("name", partOfName)
+        );
+        
+        return containers;
+    }
     
     @Test
     public void hello_world() throws Exception {
+        
+        int containersBefore = numContainers();
+        
+        // ------------------------------------------------------------------------------------------------------------
         
         ContainerCreation container = docker.createContainer(ContainerConfig.builder()
                                                                             .image("hello-world")
@@ -48,7 +85,17 @@ public class DockerClientTest {
         
         ContainerExit exit = docker.waitContainer(id);
         
+        docker.removeContainer(id);
+        
+        // ------------------------------------------------------------------------------------------------------------
+        
+        int containersAfter = numContainers();
+        
         System.out.printf("Status code = %d\n", exit.statusCode());
+        System.out.printf("containersBefore = %d\n", containersBefore);
+        System.out.printf("containersAfter  = %d\n", containersAfter);
+        
+        assertEquals(containersAfter, containersBefore);
     }
     
     DefaultDockerClient newDockerClient() {
